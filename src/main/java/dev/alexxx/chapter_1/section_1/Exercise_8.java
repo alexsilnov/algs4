9b9db17eaef0bd93e1a0a62f27cf6878fccf8a88
alexsilnov <bytejoker@gmail.com>
2023-04-27 21:34:21 +0300

    Добавил Упражнение 1.1.8

---
+++ src/main/java/dev/alexxx/chapter_1/section_1/Exercise_8.java
@@ -0,0 +1,12 @@
+package dev.alexxx.chapter_1.section_1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Exercise_8 {
+    public static void main(String[] args) {
+        System.out.println('b');
+        System.out.println('b' + 'c');
+        System.out.println((char) ('a' + 4));
+    }
+}
+
