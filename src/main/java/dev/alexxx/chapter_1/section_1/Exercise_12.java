911c95eaa85df894f2a89c456af35ab54d1f82d3
alexsilnov <bytejoker@gmail.com>
2023-04-28 23:49:07 +0300

    Добавил Упражнение 1.1.12

---
+++ src/main/java/dev/alexxx/chapter_1/section_1/Exercise_12.java
@@ -0,0 +1,20 @@
+package dev.alexxx.chapter_1.section_1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Exercise_12 {
+
+    public static void main(String[] args) {
+        int[] a = new int[10];
+        for (int i = 0; i < 10; i++) {
+            a[i] = 9 - i;
+        }
+        for (int i = 0; i < 10; i++) {
+            a[i] = a[a[i]];
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
+
