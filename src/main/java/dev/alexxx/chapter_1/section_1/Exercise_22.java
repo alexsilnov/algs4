cb8b1ee83ea6c5f2ca5feafcb77e30dde8d4faf6
alexsilnov <bytejoker@gmail.com>
2023-05-06 17:58:47 +0300

    Добавил Упражнение 1.1.22

--- src/main/java/dev/alexxx/chapter_1/section_1/Exercise_22.java
+++ src/main/java/dev/alexxx/chapter_1/section_1/Exercise_22.java
@@ -5,46 +5,51 @@ import org.apache.commons.lang3.tuple.ImmutableTriple;
 import org.apache.commons.lang3.tuple.Triple;
 
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.PrintStream;
+import java.util.*;
 
 public class Exercise_22 {
+    private static int BOUND = 100;
+    private static int NUM_TO_FIND = 23;
 
     public static void main(String[] args) {
-        List<String> formattedRows = formatRows(parseRows(readRows(System.in)));
-        formattedRows.forEach(System.out::println);
+        int[] array = random1SortedIntArray(BOUND);
+        System.out.println(Arrays.toString(array));
+        int position = rank(NUM_TO_FIND, array);
+        System.out.println(position == -1 ? "\nNumber " + NUM_TO_FIND + " not found" : "Found in position " + position);
     }
 
-    private static List<String> readRows(InputStream inputStream) {
-        Scanner scanner = new Scanner(inputStream);
-        List<String> rows = new ArrayList<>();
-        while (scanner.hasNext()) {
-            rows.add(scanner.nextLine());
-        }
-        return rows;
+
+    private static int rank(int key, int[] a) {
+        return rank(key, a, 0, a.length - 1, 0);
     }
 
-    private static List<Triple<String, Integer, Integer>> parseRows(List<String> rows) {
-        List<Triple<String, Integer, Integer>> tripleList = new ArrayList<>();
-        for (String row : rows) {
-            String[] splitRow = row.split(" ");
-            tripleList.add(new ImmutableTriple<>(splitRow[0], Integer.parseInt(splitRow[1]), Integer.parseInt(splitRow[2])));
+    private static int rank(int key, int[] a, int lo, int hi, int depth) {
+        System.out.print(" ".repeat(depth) + "lo = " + lo + " hi = " + hi);
+        if (lo > hi) return -1;
+        int mid = lo + (hi - lo) / 2;
+        System.out.println(" mid = " + mid);
+        depth++;
+        if (key < a[mid]) {
+            return rank(key, a, lo, mid - 1, depth);
+        } else {
+            if (key > a[mid]) {
+                return rank(key, a, mid + 1, hi, depth);
+            } else {
+                return mid;
+            }
         }
-        return tripleList;
     }
 
-    private static List<String> formatRows(List<Triple<String, Integer, Integer>> tripleList) {
-        List<String> formattedRows = new ArrayList<>();
-        for (Triple<String, Integer, Integer> triple : tripleList) {
-            formattedRows.add(String.format("%s %d %d %3f",
-                    triple.getLeft(),
-                    triple.getMiddle(),
-                    triple.getRight(),
-                    triple.getMiddle().doubleValue() / triple.getRight().doubleValue()
-                )
-            );
+    private static int[] random1SortedIntArray(int bound) {
+        Random random = new Random();
+        int rowLength = random.nextInt(bound);
+        int[] array = new int[rowLength];
+        for (int i = 0; i < rowLength; i++) {
+            array[i] = random.nextInt(bound);
         }
-        return formattedRows;
+        Arrays.sort(array);
+        return array;
+
     }
 }
