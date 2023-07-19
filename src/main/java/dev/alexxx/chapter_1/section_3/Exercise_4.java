52304e3a232e3424a2ee5defeb9245e5bed4a05b
alexsilnov <bytejoker@gmail.com>
2023-07-19 17:59:10 +0300

    Добавил Упражнение 1.3.4

---
+++ src/main/java/dev/alexxx/chapter_1/section_3/Exercise_4.java
@@ -0,0 +1,37 @@
+package dev.alexxx.chapter_1.section_3;
+
+
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdIn;
+
+public class Exercise_4 {
+
+    public static void main(String[] args) {
+        Stack<Character> stack = new Stack<>();
+        while (!StdIn.isEmpty()) {
+            Character str = StdIn.readChar();
+            switch (str) {
+                case '{', '[', '(' -> stack.push(str);
+                default -> {
+                    if (stack.isEmpty()) {
+                        System.out.println(false);
+                        return;
+                    }
+                    Character pop = stack.pop();
+                    boolean bool = switch (pop) {
+                        case '{' -> str.equals('}');
+                        case '[' -> str.equals(']');
+                        case '(' -> str.equals(')');
+                        default -> false;
+                    };
+                    if (!bool) {
+                        System.out.println(false);
+                        return;
+                    }
+                }
+            }
+        }
+        System.out.println(stack.isEmpty());
+    }
+
+}
