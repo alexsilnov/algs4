360ba8e1cb1b5d6dcd90ca36d272b0a5902749af
alexsilnov <bytejoker@gmail.com>
2023-07-19 17:05:05 +0300

    Добавил Упражнение 1.3.2

---
+++ src/main/java/dev/alexxx/chapter_1/section_3/Exercise_2.java
@@ -0,0 +1,16 @@
+package dev.alexxx.chapter_1.section_3;
+
+
+import edu.princeton.cs.algs4.Stack;
+
+import java.io.IOException;
+
+public class Exercise_2 {
+
+    public static void main(String[] args) throws IOException {
+        Stack.main(new String[]{});
+//        it was - the best - of times - - - it was - the - -
+//        was best times of the was the it
+    }
+
+}
