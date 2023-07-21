222a835fd8bf08aa088df7f10dfd7d78d0c92d92
alexsilnov <bytejoker@gmail.com>
2023-07-21 15:49:38 +0300

    Добавил Упражнение 1.3.9

---
+++ src/main/java/dev/alexxx/chapter_1/section_3/Exercise_9.java
@@ -0,0 +1,27 @@
+package dev.alexxx.chapter_1.section_3;
+
+
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdIn;
+
+public class Exercise_9 {
+    public static void main(String[] args) {
+        Stack<String> values = new Stack<>();
+        Stack<String> operation = new Stack<>();
+        String[] str = StdIn.readAll().split("\\s");
+        for (String s : str) {
+            switch (s) {
+                case "+", "-", "*" -> operation.push(s);
+                case ")" -> {
+                    String val2 = values.pop();
+                    String val1 = values.pop();
+                    String subValue = "(" + val1 + operation.pop() + val2 + ")";
+                    values.push(subValue);
+                }
+                default -> values.push(s);
+            }
+        }
+        System.out.println(values.pop());
+    }
+
+}
