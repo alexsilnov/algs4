7f8bde3f4e5239434ada8686931c90071f380629
alexsilnov <bytejoker@gmail.com>
2023-07-21 14:37:35 +0300

    Добавил Упражнение 1.3.7

---
+++ src/main/java/dev/alexxx/chapter_1/section_3/Exercise_7.java
@@ -0,0 +1,15 @@
+package dev.alexxx.chapter_1.section_3;
+
+
+import edu.princeton.cs.algs4.Stack;
+
+public class Exercise_7 {
+
+    public static class PeekStack<T> extends Stack<T> {
+        @Override
+        public T peek() {
+            return super.peek();
+        }
+    }
+
+}
